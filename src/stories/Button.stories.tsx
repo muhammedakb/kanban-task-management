23cf0a7ea05a66de64eecf09f53b7ab090d55027
muhammedakb <muhammetakb68@gmail.com>
2023-03-04 20:30:41 +0300

    Button & Spinner components created

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,41 +1,60 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 
-import { Button } from './Button';
+import Button from '../components/Button';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
+  title: 'Button',
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 } as ComponentMeta<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  primary: true,
-  label: 'Button',
+  size: 's',
+  text: 'Primary Button',
+  variant: 'primary',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+  size: 's',
+  text: 'Secondary Button',
+  variant: 'secondary',
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
+export const Destructive = Template.bind({});
+Destructive.args = {
+  size: 's',
+  text: 'Destructive Button',
+  variant: 'destructive',
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  size: 'small',
-  label: 'Button',
+  size: 's',
+  text: 'Small Button',
+  variant: 'primary',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'l',
+  text: 'Large Button',
+  variant: 'primary',
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+  size: 'l',
+  variant: 'primary',
+  loading: true,
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  size: 'l',
+  text: 'Disabled Button',
+  variant: 'primary',
+  disabled: true,
 };
---
+++ src/stories/Spinner.stories.tsx
@@ -0,0 +1,19 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import Spinner from '../components/Spinner';
+
+export default {
+  title: 'Spinner',
+  component: Spinner,
+} as ComponentMeta<typeof Spinner>;
+
+const Template: ComponentStory<typeof Spinner> = (args) => <Spinner {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  variant: 'primary',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  variant: 'secondary',
+};
