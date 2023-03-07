e2c6b62477d0b8af9aaefdb4513c11db34cf7562
muhammedakb <muhammetakb68@gmail.com>
2023-03-07 17:49:43 +0300

    removed unnecessary code block

--- src/components/BoardCategoryTitle/BoardCategoryTitle.tsx
+++ src/components/BoardCategoryTitle/BoardCategoryTitle.tsx
@@ -10,16 +10,14 @@ const BoardCategoryTitle = ({
   color,
   piece,
   text,
-}: BoardCategoryTitleProps) => {
-  return (
-    <section className='category__title'>
-      <span
-        className='category__title__color'
-        style={{ backgroundColor: color }}
-      />
-      <span className='category__title__text fw-700-xs'>{`${text} (${piece})`}</span>
-    </section>
-  );
-};
+}: BoardCategoryTitleProps) => (
+  <section className='category__title'>
+    <span
+      className='category__title__color'
+      style={{ backgroundColor: color }}
+    />
+    <span className='category__title__text fw-700-xs'>{`${text} (${piece})`}</span>
+  </section>
+);
 
 export default BoardCategoryTitle;
