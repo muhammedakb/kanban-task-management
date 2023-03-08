15f1a7d97c5da66c82ac3ab124791c2a51610196
muhammedakb <muhammetakb68@gmail.com>
2023-03-08 15:51:56 +0300

    Switch component

---
+++ src/components/Switch/Switch.tsx
@@ -0,0 +1,16 @@
+import './switch.scss';
+
+type SwitchProps = {
+  checked?: boolean;
+  id?: string;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+};
+
+const Switch = ({ checked, id, onChange }: SwitchProps) => (
+  <label className='switch'>
+    <input type='checkbox' id={id} checked={checked} onChange={onChange} />
+    <span className='slider round'></span>
+  </label>
+);
+
+export default Switch;
